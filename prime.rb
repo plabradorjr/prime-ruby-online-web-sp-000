2e125ec4068afe71162c2c968e3c9c7794ff051a
Pancho Labrador <plabradorjr@gmail.com>
2019-10-13 16:41:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(input)
-  if input < 0 or num == 0 or num == 1
+  if input < 0 or input == 0 or input == 1
     return false
   else
     (2..input-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+      input % possible_factor != 0
     end
   end
 end
